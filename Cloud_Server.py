f4b2709c42b825188bef25de95579bb6a015a136
Wang Jiangtong <wendywangjt@gmail.com>
2020-05-21 22:12:16 -0700

    Update Cloud Server

--- Cloud_Server.py
+++ Cloud_Server.py
@@ -4,35 +4,50 @@ import sys
 import time
 
 import Global_Var
-from Cloud_Computation import Cloud_Computation
+from src.cloud.Cloud_Computation import Cloud_Computation
 
 template = '{{ "event": "{}", "content" : "{}" }}'
 
 class Cloud_Server:
     def __init__(self):
         print("Cloud Server starts.")
+
+        #####
+        # Socket part
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REP)
-        self.socket.setsockopt(zmq.LINGER, 3000)      # The message will remain in the socket for 3 seconds if failing to send to the cloud 
+        # self.socket.setsockopt(zmq.LINGER, 3000)      # The message will remain in the socket for 3 seconds if failing to send to the cloud 
         self.socket.bind("tcp://*:%s" % Global_Var.CLOUD_PORT)
         print("Cloud Server: Bind to port:" + "tcp://*:%s" % Global_Var.CLOUD_PORT)
-        # self.computation = Cloud_Computation()
-        
+
+        #####
+        # Computation part
+        self.computation = Cloud_Computation()
         # self.CHECKOUT=False
+
+
+    
+    def getRequestFromFog(self):
+        message = self.socket.recv().decode("utf-8")
+        print("Cloud Server: Received request:", message)
+        return message
+
+    
+    def sendReplyToFog(self, reply: str):
+        self.socket.send_string(reply)
+        print("Cloud Server: Send reply to the Fog:", reply)
+
 
 
     def run(self):
         while True:
             #  Wait for next request from client
-            message = self.socket.recv().decode("utf-8")
-            print("Cloud Server: Received request:", message)
-            time.sleep(1)
+            request = self.getRequestFromFog()
 
             ## TODO: classify reply
-            # reply = self.computation(message)
-            reply = message
+            reply = self.computation(request)
             print("Cloud Server:", reply)
-            self.socket.send_string(reply)
+            
 
             # if message=="checkout???" :
             #     reply = str(self.CHECKOUT)
@@ -52,6 +67,7 @@ class Cloud_Server:
             #     self.socket.send_string("Bye")
             #     print("Cloud Server: Bye")
             #     break
+
 
 
     def __del__(self):
